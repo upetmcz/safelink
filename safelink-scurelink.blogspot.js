7c2db2066a6dddaec6fef08c2899e2442e3bb1d6
upetmcz <nurulzainuddin77@gmail.com>
2018-07-03 21:46:29 +0800

    Update safelink-scurelink.blogspot.js

--- safelink-scurelink.blogspot.js
+++ safelink-scurelink.blogspot.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="hybrid-cloud-solutions.html"}
 if(x=="2"){xxx="public-private-and-hybrid-cloud-whats.html"}
 if(x=="3"){xxx="What-Is-the-Hybrid-Cloud-and-How-Can-It-Help-Your-Business.html"}
-if(x=="4"){xxx="hybrid-cloud.html"}a_to_ve[i].href="http://scurelink.blogspot.com/2018/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="hybrid-cloud.html"}a_to_ve[i].href="https://scurelink.blogspot.com/2018/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
